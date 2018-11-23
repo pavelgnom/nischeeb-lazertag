77823dc5d8d546b419848424df1c6740731f816d
Pavel Herasimau <pavelgnom@gmail.com>
2018-11-23 20:28:23 +0300

    initial commit

---
+++ lib/nischeeb_lazertag_backend.ex
@@ -0,0 +1,18 @@
+defmodule NischeebLazertagBackend do
+  @moduledoc """
+  Documentation for NischeebLazertagBackend.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NischeebLazertagBackend.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nischeeb_lazertag_backend_test.exs
@@ -0,0 +1,8 @@
+defmodule NischeebLazertagBackendTest do
+  use ExUnit.Case
+  doctest NischeebLazertagBackend
+
+  test "greets the world" do
+    assert NischeebLazertagBackend.hello() == :world
+  end
+end
