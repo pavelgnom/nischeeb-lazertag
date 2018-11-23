9c1ef5aadcf6d7af434c4237c740f6803b65ad78
Pavel Herasimau <pavelgnom@gmail.com>
2018-11-23 23:01:35 +0300

    jkfdsjklfdsjlkfljsd

--- lib/collisions.ex
+++ lib/collisions.ex
@@ -1,5 +1,6 @@
 defmodule NischeebLazertagBackend.Collisions do
   @epsilon 0.5
+
   def handle(potential_victims, shooter) do
     angle = :math.pi() * shooter.direction / 180.0
 
@@ -8,22 +9,16 @@ defmodule NischeebLazertagBackend.Collisions do
       p2: %{x: :math.cos(angle), y: :math.sin(angle)}
     }
 
-    Enum.map(potential_victims, fn victim ->
+    Enum.map(potential_victims, fn {ip, victim} ->
       distance =
         Kernel.abs(
-          (vector[:p2][:y] - vector[:p1][:y]) * victim.x -
-            (vector[:p2][:x] - vector[:p1][:x]) * victim.y + vector[:p2][:x] * vector[:p1][:y] -
+          (vector[:p2][:y] - vector[:p1][:y]) * victim.x - (vector[:p2][:x] - vector[:p1][:x]) * victim.y + vector[:p2][:x] * vector[:p1][:y] -
             vector[:p2][:y] * vector[:p1][:x]
-        ) /
-          :math.sqrt(
-            :math.pow(vector[:p2][:y] - vector[:p1][:y], 2) +
-              :math.pow(vector[:p2][:x] - vector[:p1][:x], 2)
-          )
+        ) / :math.sqrt(:math.pow(vector[:p2][:y] - vector[:p1][:y], 2) + :math.pow(vector[:p2][:x] - vector[:p1][:x], 2))
 
-      distance_from_shooter =
-        :math.sqrt(:math.pow(shooter.x - victim.x, 2) + :math.pow(shooter.y - victim.y, 2))
+      distance_from_shooter = :math.sqrt(:math.pow(shooter.x - victim.x, 2) + :math.pow(shooter.y - victim.y, 2))
 
-      %{victim: victim, hit: distance < @epsilon, distance: distance_from_shooter}
+      %{victim: ip, hit: distance < @epsilon, distance: distance_from_shooter}
     end)
     |> Enum.sort_by(fn hit -> hit.distance end)
     |> Enum.find(& &1.hit)
--- test/collisions_test.exs
+++ test/collisions_test.exs
@@ -10,20 +10,16 @@ defmodule NischeebLazertagBackend.CollisionsTest do
       {4, 5, 6, 7} => %Player{x: 1.0, y: 1.0}
     }
 
-    shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 90.0}
-
-    assert Collisions.handle(players, shooter) == %Player{ip: {3, 4, 5, 6}, x: 0.0, y: 1.0}
-  end
-
-  test "shouldn't kill anyone if shot anywhere else" do
-    players = [
-      {2, 3, 4, 5} => %Player{x: 1.0, y: 0.0},
-      {3, 4, 5, 6} => %Player{x: 0.0, y: 1.0},
-      {4, 5, 6, 7} => %Player{x: 1.0, y: 1.0}
-    ]
-
     shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 0.0}
+    assert Collisions.handle(players, shooter) == nil
 
+    shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 90.0}
+    assert Collisions.handle(players, shooter) == {2, 3, 4, 5}
+
+    shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 180.0}
+    assert Collisions.handle(players, shooter) == {3, 4, 5, 6}
+
+    shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 270.0}
     assert Collisions.handle(players, shooter) == nil
   end
 end
