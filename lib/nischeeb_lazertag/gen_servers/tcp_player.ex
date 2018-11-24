fdd9089997c598ff52f614904b51a3c153cb5a72
Stanislav Lapata <stanislavlapata@gmail.com>
2018-11-24 19:07:56 +0300

    yeb

---
+++ lib/nischeeb_lazertag/gen_servers/tcp_player.ex
@@ -0,0 +1,43 @@
+# > echo "hello world" | nc -u -w0 localhost:2052
+defmodule NischeebLazertag.TCPPlayer do
+  use GenServer
+
+  def start_link(socket, ip) do
+    GenServer.start_link(__MODULE__, socket, name: {:global, ip})
+  end
+
+  def get_state() do
+    GenServer.call(__MODULE__, :get_state)
+  end
+
+  def init(socket) do
+    {:ok, %{socket: socket}}
+  end
+
+  def handle_info({:tcp, socket, params}, state) do
+    # {:ok, {ip, client_port}} = :inet.peername(socket)
+
+    case decode(params) do
+      %{"action" => "join", "data" => data} = params ->
+        NischeebLazertag.GenServers.Game.add_player(ip, data)
+    end
+
+    {:noreply, state}
+  end
+
+  defp decode(data) do
+    with {:ok, data} <- Jason.decode(data) do
+      data
+    else
+      {:error, _error} ->
+        IO.puts("Not json")
+
+      %{} ->
+        IO.puts("Invalid data")
+    end
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+end
---
+++ lib/nischeeb_lazertag/gen_servers/tcp_server.ex
@@ -0,0 +1,30 @@
+defmodule NischeebLazertag.TCPServer do
+  require Logger
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    {:ok, socket} = :gen_tcp.listen(2052, [:binary, packet: :line, active: true, reuseaddr: true])
+    Logger.info("Accepting connections")
+    send(__MODULE__, :loop)
+    {:ok, %{socket: socket}}
+  end
+
+  def handle_info(:loop, state) do
+    loop_acceptor(state.socket)
+
+    {:noreply, state}
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, {ip, _client_port}} = :inet.peername(client)
+    {:ok, pid} = NischeebLazertag.TCPPlayer.start_link(client, ip)
+
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+end
