eb865315706045ef1d8de84759e288a3630be64f
Nikita Vorobey <nikitav@modusgo.com>
2018-11-24 10:46:50 +0300

    Add missing test

--- test/collisions_test.exs
+++ test/collisions_test.exs
@@ -25,4 +25,15 @@ defmodule NischeebLazertagBackend.CollisionsTest do
     shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 270.0}
     assert Collisions.handle(players, shooter) == nil
   end
+
+  test "should kill only closest victim" do
+    players = %{
+      {2, 3, 4, 5} => %Player{x: 1.0, y: 0.0},
+      {3, 4, 5, 6} => %Player{x: 2.0, y: 0.0},
+      {4, 5, 6, 7} => %Player{x: 1.0, y: 1.0}
+    }
+
+    shooter = %Player{x: 0, y: 0, angle: 0.0, direction: 90.0}
+    assert Collisions.handle(players, shooter) == {2, 3, 4, 5}
+  end
 end
