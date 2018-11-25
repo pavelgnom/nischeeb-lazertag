13ffb03429dbe3a96b7f23f099ab4ab7e4433776
Stanislav Lapata <stanislavlapata@gmail.com>
2018-11-25 12:45:29 +0300

    add render html

--- lib/nischeeb_lazertag/web_ui/map_plug.ex
+++ lib/nischeeb_lazertag/web_ui/map_plug.ex
@@ -7,25 +7,33 @@ defmodule MapPlug do
   end
 
   def call(conn, _opts) do
-    players =
-      NischeebLazertag.GenServers.Game.get_state().players
-      |> Enum.map(fn {_ip, player} -> %{x: player.x, y: player.y, direction: player.direction, nickname: player.nickname, health: player.health} end)
+    if conn.request_path == "/" do
+      file = File.read!(File.cwd!() <> "/lib/nischeeb_lazertag/web_ui/index.html")
 
-    {x_min, x_max} = Enum.min_max_by(players, fn %{x: x} -> x end, fn -> {0, 500} end)
-    x_delta = x_max.x - x_min.x
+      conn
+      |> put_resp_content_type("text/html")
+      |> send_resp(200, file)
+    else
+      players =
+        NischeebLazertag.GenServers.Game.get_state().players
+        |> Enum.map(fn {_ip, player} -> %{x: player.x, y: player.y, direction: player.direction, nickname: player.nickname, health: player.health} end)
 
-    {y_min, y_max} = Enum.min_max_by(players, fn %{y: y} -> y end, fn -> {0, 500} end)
-    y_delta = y_max.y - y_min.y
+      {x_min, x_max} = Enum.min_max_by(players, fn %{x: x} -> x end, fn -> {0, 500} end)
+      x_delta = x_max.x - x_min.x
 
-    scale = 500 / Enum.max([x_delta, y_delta])
+      {y_min, y_max} = Enum.min_max_by(players, fn %{y: y} -> y end, fn -> {0, 500} end)
+      y_delta = y_max.y - y_min.y
 
-    scaled =
-      players
-      |> Enum.map(fn player -> %{player | x: player.x * scale, y: player.y * scale, direction: player.direction * :math.pi() / 180} end)
-      |> Jason.encode!()
+      scale = 500 / Enum.max([x_delta, y_delta])
 
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, scaled)
+      scaled =
+        players
+        |> Enum.map(fn player -> %{player | x: player.x * scale, y: player.y * scale, direction: player.direction * :math.pi() / 180} end)
+        |> Jason.encode!()
+
+      conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, scaled)
+    end
   end
 end
