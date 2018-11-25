34dd7ad39c1bc13f989fe341df2120b73fe2c09a
Stanislav Lapata <stanislavlapata@gmail.com>
2018-11-25 13:15:38 +0300

    change canvas logic

--- lib/nischeeb_lazertag/web_ui/map_plug.ex
+++ lib/nischeeb_lazertag/web_ui/map_plug.ex
@@ -16,7 +16,11 @@ defmodule MapPlug do
     else
       players =
         NischeebLazertag.GenServers.Game.get_state().players
-        |> Enum.map(fn {_ip, player} -> %{x: player.x, y: player.y, direction: player.direction, nickname: player.nickname, health: player.health} end)
+        |> Enum.map(fn {_ip, player} ->
+          %{x: player.x, y: -player.y, direction: player.direction, nickname: player.nickname, health: player.health}
+        end)
+
+      # players = [%{direction: 225, nickname: "90", x: 30, y: -30, health: 100}, %{direction: 20, nickname: "0", x: 0, y: 0, health: 100}]
 
       {x_min, x_max} = Enum.min_max_by(players, fn %{x: x} -> x end, fn -> {0, 500} end)
       x_delta = x_max.x - x_min.x
@@ -24,7 +28,7 @@ defmodule MapPlug do
       {y_min, y_max} = Enum.min_max_by(players, fn %{y: y} -> y end, fn -> {0, 500} end)
       y_delta = y_max.y - y_min.y
 
-      scale = 500 / Enum.max([x_delta, y_delta])
+      scale = 650 / Enum.max([x_delta, y_delta])
 
       scaled =
         players
